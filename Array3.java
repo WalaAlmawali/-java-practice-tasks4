c9d9867f0a895abc88818e7e0d26e8295bc16532
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-08 14:45:26 +0400

    add task 17 isEverywhere

--- Array3.java
+++ Array3.java
@@ -1,8 +1,14 @@
 public class Array3 {
     public static void main(String[] args){
         Array3 ex = new Array3();
-        int [] arr = {1,1,1,4};
-        System.out.println(ex.only14(arr));
+        // task 16
+       /* int [] arr = {1,1,1,4};
+        System.out.println(ex.only14(arr));*/
+
+        // task 17
+        int [] arr = {1,2,1,3};
+        System.out.println(ex.isEverywhere(arr,2));
+
     }
 
     public boolean only14(int [] arr){
@@ -10,6 +16,15 @@ public class Array3 {
         for(int i = 0 ; i < arr.length; i++){
             if(arr[i] != 1 && arr[i] !=4){
                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isEverywhere(int [] arr, int n){
+        for (int i =0 ; i<arr.length-1;i++){
+            if(arr[i] != n && arr[i+1] != n){
+                return false;
             }
         }
         return true;
